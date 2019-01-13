f6b2a4f3f569e489966a7b987699e2bcb0b0461f
khllkcm <khalilqacem@gmail.com>
2019-01-13 23:36:59 +0100

    Add input ranges

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
+var a_elem = document.getElementById("a");
+
+var rangeValuea = function(){
+  var newValue = a_elem.value;
+  var target = document.querySelector('.avalue');
+  target.innerHTML = newValue;
+}
+
+a_elem.addEventListener("input", rangeValuea);
+
+
+var b_elem = document.getElementById("b");
+
+var rangeValueb = function(){
+  var newValue = b_elem.value;
+  var target = document.querySelector('.bvalue');
+  target.innerHTML = newValue;
+}
+
+b_elem.addEventListener("input", rangeValueb);
+
+
 plotST()
 plotRW()
 
@@ -30,12 +52,9 @@ function plotST() {
 
   //input a and b
 
-  var a_input = document.getElementById("a")
-  var b_input =  document.getElementById("a")
 
-  var a = -a_input.value
-  var b = b_input.value
-
+ var a = -1;
+ var b =1;
   //calculate tau
   var tau = -a*b;
 
@@ -157,99 +176,149 @@ function plotST() {
 
 
 
-function plotRW(){
 
-  function draw(data){
-        var num = Math.random();
-        if(num<0.5) {
-            data.push({Sum:data[data.length-1].Sum-1, Round:data[data.length-1].Round+1});
-        } else {
-            data.push({Sum:data[data.length-1].Sum+1, Round:data[data.length-1].Round+1});
-        }
-    }
+function plotRW() {
 
-  var data = [{Sum:0, Round:0}]
-          while(data[data.length-1].Sum < 3 && data[data.length-1].Sum > -3) {
-            draw(data);
-          }
-          data.push({Sum:data[data.length-1].Sum, Round:data[data.length-1].Round+1})
-          drawChart(data);
 
-  d3.select('#draw').on("click",function() {
-          var data = [{Sum:0, Round:0}]
-          while(data[data.length-1].Sum < 3 && data[data.length-1].Sum > -3) {
-            draw(data);
-          }
-          data.push({Sum:data[data.length-1].Sum, Round:data[data.length-1].Round+1})
-          drawChart(data);
+ var a = -1;
+ var b =1;
+
+  function draw(data) {
+    var num = Math.random();
+    if (num < 0.5) {
+      data.push({
+        Sum: data[data.length - 1].Sum - 1,
+        Round: data[data.length - 1].Round + 1
       });
+    } else {
+      data.push({
+        Sum: data[data.length - 1].Sum + 1,
+        Round: data[data.length - 1].Round + 1
+      });
+    };
+  };
+
+  simRW();
+
+
+  function simRW() {
+  var data = [{
+      Sum: 0,
+      Round: 0
+    }];
+    while (data[data.length - 1].Sum < b && data[data.length - 1].Sum > a) {
+      draw(data);
+    }
+    data.push({
+      Sum: data[data.length - 1].Sum,
+      Round: data[data.length - 1].Round + 1
+    });
+    drawChart(data);
+  };
+
+
+
+
+
+  d3.select('#draw').on("click", function () {
+     
+     simRW();
+  });
+
+  d3.select("#a").on("input", function() {
+    a = -this.value;
+    simRW();
+  });
+
+
+  d3.select("#b").on("input", function() {
+    b = +this.value;
+    simRW();
+  });
 
 
   function drawChart(data) {
-  d3.selectAll("#plotRW > *").remove();
-  var pad = 30;
-  var margin = {top: 40, right: 40, bottom: 40, left: 40};
-  var width = 300 - margin.left - margin.right;
-  var height = 200 - margin.top - margin.bottom;
+    d3.selectAll("#plotRW > *").remove();
+    var pad = 30;
+    var margin = {
+      top: 40,
+      right: 40,
+      bottom: 40,
+      left: 40
+    };
+    var width = 300 - margin.left - margin.right;
+    var height = 200 - margin.top - margin.bottom;
 
-  var svg = d3.select("#plotRW").append("svg").attr("width", "100%")
-          .attr("height", "100%")
-          .attr("viewBox", "0 0 " + (width + margin.left + margin.right) + " " + (height + margin.top + margin.bottom))
-          .attr("preserveAspectRatio", "xMidYMid meet")
-          .append("g")
-          .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    var svg = d3.select("#plotRW").append("svg").attr("width", "100%")
+      .attr("height", "100%")
+      .attr("viewBox", "0 0 " + (width + margin.left + margin.right) + " " + (height + margin.top + margin.bottom))
+      .attr("preserveAspectRatio", "xMidYMid meet")
+      .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-  var g = svg.append("g");
-
-
-  var roundmax = d3.max(data, function(d) { return d.Round; });
-      summin = d3.min(data, function(d) { return d.Sum; });
-      summax =d3.max(data, function(d) { return d.Sum; });
+    var g = svg.append("g");
 
 
-  var x = d3.scaleLinear()
-      .domain(d3.extent(data, function(d) { return +d.Round }))
+    var roundmax = d3.max(data, function (d) {
+      return d.Round;
+    });
+    summin = d3.min(data, function (d) {
+      return d.Sum;
+    });
+    summax = d3.max(data, function (d) {
+      return d.Sum;
+    });
+
+
+    var x = d3.scaleLinear()
+      .domain(d3.extent(data, function (d) {
+        return +d.Round
+      }))
       .range([0, width]);
-      
-  var y = d3.scaleLinear()
-      .domain(d3.extent(data, function(d) { return +d.Sum }))
+
+    var y = d3.scaleLinear()
+      .domain(d3.extent(data, function (d) {
+        return +d.Sum
+      }))
       .range([height, 0]);
 
 
-
-
-  var line = d3.line()
-      .x(function(d) { return x(d.Round)})
-      .y(function(d) { return y(d.Sum)})
+    var line = d3.line()
+      .x(function (d) {
+        return x(d.Round)
+      })
+      .y(function (d) {
+        return y(d.Sum)
+      })
       .curve(d3.curveStepAfter);
 
 
-  g.append("g")
-        .attr("class", "axis axis--x")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x).ticks(Math.min(10,roundmax), "s"));
+    g.append("g")
+      .attr("class", "axis axis--x")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x).ticks(Math.min(10, roundmax), "s"));
 
-  g.append("g")
+    g.append("g")
       .attr("class", "axis axis--y")
       .attr("transform", "translate(0,0)")
-      .call(d3.axisLeft(y).ticks(summax-summin, "s"));
+      .call(d3.axisLeft(y).ticks(summax - summin, "s"));
 
-  g.append("text")
-        .text("round")
-        .style("font", "14px times")
-        .attr("class", "titles")
-        .attr("transform", "translate(" + (width - margin.left)/ 2 + "," + (height + pad) + ")")
-        .attr("alignment-baseline","hanging"); 
+    g.append("text")
+      .text("round")
+      .style("font", "14px times")
+      .attr("class", "titles")
+      .attr("transform", "translate(" + (width - margin.left) / 2 + "," + (height + pad) + ")")
+      .attr("alignment-baseline", "hanging");
 
-  g.append("text")
-        .text("sum")
-        .style("font", "14px times")
-        .attr("class", "titles")
-        .attr("transform", "translate(" + -pad/1.5 + "," + (height+margin.top) / 2 + ")rotate(-90)")
-        .attr("alignment-baseline","baseline");  
+    g.append("text")
+      .text("sum")
+      .style("font", "14px times")
+      .attr("class", "titles")
+      .attr("transform", "translate(" + -pad / 1.5 + "," + (height + margin.top) / 2 + ")rotate(-90)")
+      .attr("alignment-baseline", "baseline");
 
 
-  var path = g.append("path")
+    var path = g.append("path")
       .datum(data)
       .attr("fill", "none")
       .attr("stroke", "steelblue")
@@ -259,24 +328,23 @@ function plotRW(){
       .attr("d", line)
       .style("display", null);
 
-   var totalLength = path.node().getTotalLength();
+    var totalLength = path.node().getTotalLength();
 
-   function startdraw(){
-    path
-      .attr("stroke-dasharray", totalLength + " " + totalLength)
-      .attr("stroke-dashoffset", totalLength)
-      .style("display", null) 
-      .transition()
+    function startdraw() {
+      path
+        .attr("stroke-dasharray", totalLength + " " + totalLength)
+        .attr("stroke-dashoffset", totalLength)
+        .style("display", null)
+        .transition()
         .duration(2000)
-        .attr("stroke-dashoffset", 0); 
-   }
+        .attr("stroke-dashoffset", 0);
+    };
 
-  startdraw()
+    startdraw();
 
-
-    d3.select("#button").on("click", function() {
-        startdraw()
-      });
+    d3.select("#button").on("click", function () {
+      startdraw();
+    });
 
 
   }
